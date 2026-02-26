20c1243aa1ed48c041f771818d8b1d215bd89eba
Вячеслав Прохода <Wjascheslaw88@gmail.com>
2026-02-26 08:53:39 +0400

    1.2

--- Frontend/chat-ai/src/pages/Chat_users/Chat_users.js
+++ Frontend/chat-ai/src/pages/Chat_users/Chat_users.js
@@ -1,12 +1,13 @@
 import React from "react";
+import css from "./Chat_users.module.css";
 
 const Chat_user = () => {
-    return(
-     <div>
-        
-     </div>
-
-    )
-}
+    return (
+        <div className={css.wrapper}>
+            <div className={css.user}></div>
+            <div className={css.chat}></div>
+        </div>
+    );
+};
 
 export default Chat_user;
--- Frontend/chat-ai/src/store/index.js
+++ Frontend/chat-ai/src/store/index.js
@@ -2,8 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 
 const reduser = combineReducers({
-   
-    
+
 });
+
 const store = createStore(reduser, applyMiddleware(thunk));
+
 export default store;
