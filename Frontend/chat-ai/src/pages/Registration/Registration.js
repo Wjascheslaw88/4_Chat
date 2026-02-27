f9de8295cc2ca7860c8cbe3a58b4be3ace5e6897
Вячеслав Прохода <Wjascheslaw88@gmail.com>
2026-02-27 12:33:46 +0400

    1.6

--- Frontend/chat-ai/src/pages/Registration/Registration.js
+++ Frontend/chat-ai/src/pages/Registration/Registration.js
@@ -1,8 +1,25 @@
-
+import { useState } from "react";
 import css from "./Registration.module.css"
 import { Button, Input } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const Registration = () => {
+    
+    const navigate = useNavigate()
+    const [ nickName, setNickName ] = useState('');
+    const [ chatName, setChatName ] = useState('');
+
+    const handleNickName = (e) => {
+    setNickName(e.target.value);
+};
+    const handleChatName = (e) => {
+    setChatName(e.target.value);
+
+};
+
+    const hendleChat = () => {
+        navigate(`/${chatName}`)
+    }
 
     return (
         <>
@@ -16,7 +33,8 @@ const Registration = () => {
                     type="text" 
                     placeholder="Enter nickname" 
                     className={css.NickName}
-                    value={''}
+                    value={nickName}
+                    onChange={handleNickName}
                     />
                     
                     <label className={css.label}>Chat name</label>
@@ -26,14 +44,15 @@ const Registration = () => {
                     type="text" 
                     placeholder="Enter the chat name" 
                     className={css.ChatName} 
-                    value={''}
+                    value={chatName}
+                    onChange={handleChatName}
                     />
                     
 
-                    <Button className={css.Button}>Войти</Button>
+                    <Button onClick={hendleChat} className={css.Button}>Войти</Button>
                 </div>
             </div>
-            <Button className={css.buttonAI}>Chat AI</Button>
+            <Button  className={css.buttonAI}>Chat AI</Button>
         </>
     )
 }
--- Frontend/chat-ai/src/pages/routes.js
+++ Frontend/chat-ai/src/pages/routes.js
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useParams } from "react-router-dom"
 import ChatUser from "./Chat_users/Chat_users"
 import Registration from "./Registration/Registration"
 import PageNotFound from "./Page_not_found/Page_not_found"
 
 
 const Router=()=> {
+
+
     return (
         <Routes>
             <Route path="/" element={<Registration />}></Route>
